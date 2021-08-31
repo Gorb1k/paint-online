b1601a9c9ba72cce302dc6695ec58c0b7d7dbd72
Ihor Horb <gorbigorolegovich@gmail.com>
2021-08-31 18:27:37 +0300

    btns in progress

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -4,7 +4,7 @@ import "../styles/toolbar.scss"
 const ToolBar = () => {
     return (
         <div className='toolbar'>
-
+            <button className='toolbar__btn brush'/>
         </div>
     );
 };
