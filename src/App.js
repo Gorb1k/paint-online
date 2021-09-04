57a10239282b40fe724e3c9da2da87a3adee4b76
Ihor Horb <gorbigorolegovich@gmail.com>
2021-09-04 14:44:08 +0300

    ws connection (in progress)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import "./styles/app.scss"
 import ToolBar from "./components/ToolBar";
 import SettingsBar from "./components/SettingsBar";
 import Canvas from "./components/Canvas";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className='app'>
-            <ToolBar/>
-            <SettingsBar/>
-            <Canvas/>
+        <BrowserRouter>
+            <div className='app'>
+                <Switch>
+                    <Route path='/:id'>
+                        <ToolBar/>
+                        <SettingsBar/>
+                        <Canvas/>
+                    </Route>
+                    <Redirect to={`f${(+new Date).toString(16)}`}/>
+                </Switch>
         </div>
-    )
+</BrowserRouter>
+)
 }
 
 export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,23 +1,62 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import '../styles/canvas.scss'
 import {observer} from "mobx-react-lite";
 import canvasState from "../store/canvasState";
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import {Button, Modal} from "react-bootstrap";
+import {useParams} from "react-router-dom";
 
 const Canvas = observer( () => {
     const canvasRef = useRef()
+    const [usernameField, setUsernameField] = useState('')
+    const [modal, setModal] = useState(true)
+    const params = useParams()
+
     useEffect(() => {
         canvasState.setCanvas(canvasRef.current)
         toolState.setTool(new Brush(canvasRef.current))
     },[])
+    useEffect(() => {
+        if (canvasState.username) {
+            const socket = new WebSocket('ws://localhost:5000/')
+            socket.onopen = () => {
+                console.log('подключение установлено')
+                socket.send(JSON.stringify({
+                    id: params.id,
+                    username: canvasState.username,
+                    method: 'connection'
+                }))
+            }
+            socket.onmessage = (e) => {
+                console.log(e.data)
+            }
+        }
+    },[canvasState.username])
 
     const mouseDownHandler = () => {
         canvasState.pushToUndo(canvasRef.current.toDataURL())
     }
+    const connectionHandler = () => {
+      canvasState.setUsername(usernameField)
+        setModal(false)
+    }
 
     return (
         <div className="canvas">
+            <Modal show={modal} onHide={() => {}}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Введите ваше имя</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <input type="text" value={usernameField} onChange={(event) => setUsernameField(event.target.value)}/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={connectionHandler}>
+                        Войти
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <canvas onMouseDown={mouseDownHandler} ref={canvasRef} width={600} height={400}/>
         </div>
     );
